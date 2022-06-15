bb1a5f4aa835a01b724b33b0ef8dfe9006764398
asabino <asabino2@gmail.com>
2022-06-15 15:56:37 -0300

    mudança de versão ajuste para matar o processo do streamlink app após desconexão do cliente

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 const { spawn } = require('child_process');
 const express = require('express');
+var config = {};
+try {
+    const fs = require("fs");
+    const jsonString = fs.readFileSync("./config.json");
+    config = JSON.parse(jsonString);
+} catch (err) {
+    config = { port: 3000, stramlinkpath: "", ffmpegpath: "" };
+}
+//console.log("port: " + config.port);
 
 
 const app = express();
@@ -9,15 +18,16 @@ var ffmpegapp = "ffmpeg";
 var ippublic = "";
 var publicip = "44";
 
+if (config.streamlinkpath == undefined) {
+    config.streamlinkpath = "";
+}
 
-if (process.argv[3] != undefined) {
-    streamlinkapp = process.argv[3];
+if (config.ffmpegpath == undefined) {
+    config.ffmpegpath = "";
 }
 
 
-if (portlisten == undefined) {
-    portlisten = 3000;
-}
+
 
 console.log("\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557  \u2588\u2588\u2557\u2588\u2588\u2557   \u2588\u2588\u2557\r\n\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551    \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2588\u2588\u2557\u255A\u2588\u2588\u2557\u2588\u2588\u2554\u255D\u255A\u2588\u2588\u2557 \u2588\u2588\u2554\u255D\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551    \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551   \u2588\u2588\u2551 \u255A\u2588\u2588\u2588\u2554\u255D  \u255A\u2588\u2588\u2588\u2588\u2554\u255D \r\n\u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u255D  \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551    \u2588\u2588\u2554\u2550\u2550\u2550\u255D \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551   \u2588\u2588\u2551 \u2588\u2588\u2554\u2588\u2588\u2557   \u255A\u2588\u2588\u2554\u255D  \r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551    \u2588\u2588\u2551     \u2588\u2588\u2551  \u2588\u2588\u2551\u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2554\u255D \u2588\u2588\u2557   \u2588\u2588\u2551   \r\n\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D    \u255A\u2550\u255D     \u255A\u2550\u255D  \u255A\u2550\u255D \u255A\u2550\u2550\u2550\u2550\u2550\u255D \u255A\u2550\u255D  \u255A\u2550\u255D   \u255A\u2550\u255D   ");
 console.log("                                         A Proxy for the livestreams         ")
@@ -28,18 +38,39 @@ console.log("                                     Developed by Alexander Sabino 
 console.log("");
 console.log("starting...")
 console.log("");
-console.log("streamproxy is listening on port ", portlisten)
+console.log("streamproxy is listening on port ", config.port)
 app.get('/videostream/streamlink', (req, res) => {
     url = req.query.url;
-    const stream = spawn(streamlinkapp, [url, 'best', '--stdout']);
+    const stream = spawn(config.streamlinkpath + "streamlink", [url, 'best', '--stdout']);
     stream.stdout.pipe(res);
     stream.stderr.pipe(process.stderr);
+    //console.log("ended");
+    stream.on('spawn', () => {
+        console.log("playing... (" + stream.pid + ")");
+
+    })
+    stream.on('close', (code, signal) => {
+        console.log("close stop code " + code + " signal " + signal);
+    })
+
+    stream.on('exit', (code, signal) => {
+        console.log("exit stop code " + code + " signal " + signal);
+    })
+
+    req.on('close', () => {
+        console.log("closed connection");
+        stream.kill();
+    })
+
 });
+
+
+
 
 app.get('/videostream/ffmpeg', (req, res) => {
     url = req.query.url;
 
-    const stream = spawn('ffmpeg', ['-loglevel', 'fatal', '-i', url, '-vcodec', 'mpeg2video', '-acodec', 'aac', '-b', '15000k', '-strict', '-2', '-mbd', 'rd', '-copyinkf', '-flags', '+ilme+ildct', '-fflags', '+genpts', '-metadata', 'service_provider=streamproxy', '-f', 'mpegts', '-tune', 'zerolatency', '-']);
+    const stream = spawn(config.ffmpegpath + 'ffmpeg', ['-loglevel', 'fatal', '-i', url, '-vcodec', 'mpeg2video', '-acodec', 'aac', '-b', '15000k', '-strict', '-2', '-mbd', 'rd', '-copyinkf', '-flags', '+ilme+ildct', '-fflags', '+genpts', '-metadata', 'service_provider=streamproxy', '-f', 'mpegts', '-tune', 'zerolatency', '-']);
 
     stream.stdout.pipe(res);
     stream.stderr.pipe(process.stderr);
@@ -54,7 +85,7 @@ app.get('/videostream/ffmpeg', (req, res) => {
 app.get('/videostream/info', (req, res) => {
     url = req.query.url;
     var returncommand = "";
-    var commandffmpeg = "ffprobe -v quiet -print_format json -show_format -show_streams " + url;
+    var commandffmpeg = config.ffmpegpath + "ffprobe -v quiet -print_format json -show_format -show_streams " + url;
     var status = "";
     var erro = "";
 
@@ -72,7 +103,7 @@ app.get('/videostream/info', (req, res) => {
 
 app.get('/videostream/checkstream', (req, res) => {
     url = req.query.url;
-    var commandffmpeg = "ffmpeg -ss 00:00:01 -i \"" + url + "\" -vframes 1 -q:v 2 -f null -";
+    var commandffmpeg = config.ffmpegpath + "ffmpeg -ss 00:00:01 -i \"" + url + "\" -vframes 1 -q:v 2 -f null -";
     var status = "";
     var erro = "";
 
@@ -86,9 +117,6 @@ app.get('/videostream/checkstream', (req, res) => {
     res.json({ streamurl: url, streamstatus: status, mensagem: erro });
 });
 
-app.get('/', (req, res) => {
-    res.send('<h1>Stream Get<h1><br><br>See asabino2/streamget in docker hub for more info')
-});
 
 
 
@@ -98,7 +126,36 @@ app.get('/info', (req, res) => {
 
 
 
-app.listen(portlisten);
+
+
+app.get('/', async function(req, res) {
+    const https = require("https");
+    https.get('https://asabino2.github.io/streamproxy.html', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            //console.log("response data: " + data);
+            res.send(data);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+
+    });
+
+});
+
+
+
+
+app.listen(config.port);
 
 function announceServers() {
     var http = require('http');
@@ -108,4 +165,9 @@ function announceServers() {
             console.log("server on internet http://" + publicip + ":" + portlisten + "/");
         });
     });
+}
+
+
+function loadconfig() {
+
 }
